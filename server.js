69249f4c59e6b4f2d7e73b760c64d1591e8feae1
Mayur Mandani <mmandani9@gmail.com>
2024-09-22 22:50:18 +0530

    Added the middleware helment and certificate and key

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 const https = require('https');
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
   return res.send('Your personal secret value is 42!');
